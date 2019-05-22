9bd664715fbf22b61b4786388f654e25de76b152
Andy Fusniak <andy@andyfusniak.com>
2019-05-22 15:30:13 +0100

    Fix catalog get ouput to include segment

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo $ECOM_CLI_VERSION
+echo $ECOM_DOCS_PUBLIC_DOWNLOAD
 GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION
 GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION-darwin
 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION-windows
+
+cp build/ecom-$ECOM_CLI_VERSION $ECOM_DOCS_PUBLIC_DOWNLOAD/linux/ecom
+cp build/ecom-$ECOM_CLI_VERSION-darwin $ECOM_DOCS_PUBLIC_DOWNLOAD/mac/ecom
+cp build/ecom-$ECOM_CLI_VERSION-windows $ECOM_DOCS_PUBLIC_DOWNLOAD/windows/ecom.exe
--- cmd/catalog_get.go
+++ cmd/catalog_get.go
@@ -13,32 +13,24 @@ import (
 var catalogGetCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Get the catalog hierarchy",
-	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		current := rc.Configurations[currentConfigName]
-		fmt.Printf("%v\n", current)
-		ecomClient := eclient.New(current.Endpoint, timeout)
-		err := ecomClient.SetToken(&current)
-		if err != nil {
+		client := eclient.New(current.Endpoint, timeout)
+		if err := client.SetToken(&current); err != nil {
 			log.Fatal(err)
 		}
-
-		root, err := ecomClient.GetCatalog()
+		root, err := client.GetCatalog()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
-
 		treeView(root, 0, false)
-		//root := nestedset.BuildTree(nodes)
-
-		//root.PreorderTraversalPrint(os.Stdout)
-
 	},
 }
 
 func treeView(node *firebase.Category, depth int, lastSibling bool) {
-	//fmt.Printf("node.Name=%s last sibling=%t\n", node.Name, lastSibling)
+	// fmt.Printf("%+v\n", node)
+	// fmt.Printf("node.Name=%s last sibling=%t\n", node.Name, lastSibling)
 	var arm string
 	if lastSibling {
 		arm = "└── "
@@ -50,13 +42,13 @@ func treeView(node *firebase.Category, depth int, lastSibling bool) {
 	} else if depth == 1 {
 		fmt.Print(arm)
 	} else {
-		fmt.Print("│    ")
+		fmt.Print("│   ")
 		for i := 0; i < depth-2; i++ {
 			fmt.Print("    ")
 		}
 		fmt.Print(arm)
 	}
-	fmt.Printf("%s\n", node.Name)
+	fmt.Printf("%s (%s)\n", node.Segment, node.Name)
 	lastIdx := len(node.Nodes) - 1
 	for i, n := range node.Nodes {
 		treeView(n, depth+1, lastIdx == i)
