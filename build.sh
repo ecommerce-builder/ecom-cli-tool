1b60e59bdaa1d65f6734847773f7158170b2a324
Andy Fusniak <andy@andyfusniak.com>
2019-05-22 17:57:38 +0100

    Revised build process

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 echo $ECOM_CLI_VERSION
 echo $ECOM_DOCS_PUBLIC_DOWNLOAD
-GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION
+GOOS=linux   GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION-linux
 GOOS=darwin  GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION-darwin
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION-windows
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$ECOM_CLI_VERSION" -o build/ecom-$ECOM_CLI_VERSION.exe
 
-cp build/ecom-$ECOM_CLI_VERSION $ECOM_DOCS_PUBLIC_DOWNLOAD/linux/ecom
-cp build/ecom-$ECOM_CLI_VERSION-darwin $ECOM_DOCS_PUBLIC_DOWNLOAD/mac/ecom
-cp build/ecom-$ECOM_CLI_VERSION-windows $ECOM_DOCS_PUBLIC_DOWNLOAD/windows/ecom.exe
+# Linux
+rm -f $ECOM_DOCS_PUBLIC_DOWNLOAD/linux/ecom*
+cp build/ecom-$ECOM_CLI_VERSION-linux $ECOM_DOCS_PUBLIC_DOWNLOAD/linux/ecom-$ECOM_CLI_VERSION-linux
+
+
+# Mac
+rm -f $ECOM_DOCS_PUBLIC_DOWNLOAD/mac/ecom*
+cp build/ecom-$ECOM_CLI_VERSION-darwin $ECOM_DOCS_PUBLIC_DOWNLOAD/mac/ecom-$ECOM_CLI_VERSION-darwin
+
+# Windows
+rm -f $ECOM_DOCS_PUBLIC_DOWNLOAD/windows/ecom*
+cp build/ecom-$ECOM_CLI_VERSION.exe $ECOM_DOCS_PUBLIC_DOWNLOAD/windows/ecom-$ECOM_CLI_VERSION.exe
